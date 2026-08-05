5d5fab996ed41052d008ebfc954f5e6a839ddfb3
afcavf54-cmd <afcavf54@gmail.com>
2026-08-05 16:35:40 +0200

    Update route.ts

--- saas/dashboard/app/api/sites/[siteId]/blog/route.ts
+++ saas/dashboard/app/api/sites/[siteId]/blog/route.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
+export const maxDuration = 60
+export const dynamic = 'force-dynamic'
 import { getFile, listDir } from '../../../../../lib/github'
 
 /**
